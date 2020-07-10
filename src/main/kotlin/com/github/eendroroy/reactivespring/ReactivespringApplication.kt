8de27eff68dc916a93cab3c5749d5e8c8843c769
indrajit <eendroroy@gmail.com>
2020-07-10 15:33:19 +0600

    init

---
+++ src/main/kotlin/com/github/eendroroy/reactivespring/ReactivespringApplication.kt
@@ -0,0 +1,11 @@
+package com.github.eendroroy.reactivespring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactivespringApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactivespringApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/eendroroy/reactivespring/ReactivespringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.eendroroy.reactivespring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
