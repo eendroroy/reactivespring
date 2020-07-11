b82364085c57e014b0d8662566b6036dff12f131
indrajit <eendroroy@gmail.com>
2020-07-11 17:30:40 +0600

    refactor

--- src/main/kotlin/com/github/eendroroy/reactivespring/ReactivespringApplication.kt
+++ src/main/kotlin/com/github/eendroroy/reactivespring/ReactivespringApplication.kt
@@ -3,9 +3,12 @@ package com.github.eendroroy.reactivespring
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * @author indrajit
+ */
 @SpringBootApplication
 class ReactivespringApplication
 
 fun main(args: Array<String>) {
-	runApplication<ReactivespringApplication>(*args)
+    runApplication<ReactivespringApplication>(*args)
 }
